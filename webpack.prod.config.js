fd259389ceaec6dbef01cafda3b60cde656ec1ea
Aaron Panchal <aaron.panchal@appfolio.com>
2016-06-10 17:57:17 -0700

    Make react smaller in prod take 3

---
+++ webpack.prod.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: 'public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+  ]
+}
