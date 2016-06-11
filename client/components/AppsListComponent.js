397ffae6bac63789ddbc6e703050b7a455d4e244
Aaron Panchal <aaron.panchal@appfolio.com>
2016-06-10 19:47:42 -0700

    Add button for adding new apps

--- client/components/AppsListComponent.js
+++ client/components/AppsListComponent.js
@@ -6,15 +6,23 @@ import Application from './ApplicationComponent';
 
 class AppsList extends React.Component {
   render() {
-    return (
-      <ul className="app-list">
-        {this.props.apps.map((item, index) => {
+
+    let items = this.props.apps.length > 0 ?
+      this.props.apps.map((item, index) => {
           return (
             <li key={index} className="app-list__item">
               <Application {...item} />
             </li>
           );
-        })}
+      }) :
+      (<li className="app-list__item app-list__message">There aren't any apps!</li>);
+
+    return (
+      <ul className="app-list">
+        {items}
+        <li className="app-list__item app-list__message">
+          <button className="app-list__button">Add An Application</button>
+        </li>
       </ul>
     )
   }
