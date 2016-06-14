7d0c62da51c9b8e56b61777cc99c4372a1faa026
Aaron Panchal <aaron.panchal@appfolio.com>
2016-06-14 11:22:45 -0700

    Fix socket protocol for prod

--- client/index.js
+++ client/index.js
@@ -7,8 +7,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
 import Nes from 'nes';
+import config from '../config';
 
-let client = new Nes.Client('ws://' + location.host);
+let client = new Nes.Client(config.socketProtocol + location.host);
 
 client.connect(() => {
   client.request('/apps', function (err, payload) {
--- config/index.js
+++ config/index.js
@@ -2,17 +2,20 @@ const configs = {
   dev: {
     env: 'dev',
     port: 3000,
-    mongodbURI: 'mongodb://localhost:27017/hercules-dev'
+    mongodbURI: 'mongodb://localhost:27017/hercules-dev',
+    socketProtocol: 'ws://'
   },
   test: {
     env: 'test',
     port: 1234,
-    mongodbURI: 'mongodb://localhost:27017/hercules-test'
+    mongodbURI: 'mongodb://localhost:27017/hercules-test',
+    socketProtocol: 'ws://'
   },
   production: {
     env: 'production',
     port: process.env.PORT,
     mongodbURI: process.env.MONGODB_URI
+    socketProtocol: 'wss://'
   }
 }
 
