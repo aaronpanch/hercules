06d303f5b0c024f65f7e90877cb6d976aaa7ee8e
Aaron Panchal <aaron.panchal@appfolio.com>
2016-07-14 10:47:02 -0700

    Add koa-logger for dev

--- server.js
+++ server.js
@@ -1,53 +1,69 @@
 'use strict';
 
-const config = require('./config');
-const Hapi = require('hapi');
+// Middleware
+const logger = require('koa-logger');
 
-const server = new Hapi.Server();
-server.connection({ port: config.port });
 
-server.register(
-  [
-    require('nes'),
-    require('inert'),
-    {
-      register: require('./src/mongodbPlugin'),
-      options: {
-        dbURI: config.mongodbURI
-      }
-    },
-    require('./src/appsPlugin'),
-    {
-      register: require('./src/slackPlugin'),
-      options: {
-        slackDeployToken: config.slackDeployToken,
-        hostURL: config.hostURL
-      }
-    }
-  ],
-  (err) => {
-    server.route({
-      method: 'GET',
-      path: '/{param*}',
-      handler: {
-        directory: {
-          path: 'public'
-        }
-      }
-    })
+const koa = require('koa');
+let app = koa();
 
-    if (err) {
-      console.error('Failed to load plugin: ', err);
-    }
+if (app.env === 'development')
+  app.use(logger());
 
-    server.start((err) => {
-      if (err) {
-        throw err;
-      }
+app.use(function *(){
+  this.body = 'Hello World';
+});
 
-      console.log('Server running at:', server.info.uri);
-    });
-  }
-);
+app.listen(3000);
 
-module.exports = server;
+// const config = require('./config');
+// const Hapi = require('hapi');
+
+// const server = new Hapi.Server();
+// server.connection({ port: config.port });
+
+// server.register(
+//   [
+//     require('nes'),
+//     require('inert'),
+//     {
+//       register: require('./src/mongodbPlugin'),
+//       options: {
+//         dbURI: config.mongodbURI
+//       }
+//     },
+//     require('./src/appsPlugin'),
+//     {
+//       register: require('./src/slackPlugin'),
+//       options: {
+//         slackDeployToken: config.slackDeployToken,
+//         hostURL: config.hostURL
+//       }
+//     }
+//   ],
+//   (err) => {
+//     server.route({
+//       method: 'GET',
+//       path: '/{param*}',
+//       handler: {
+//         directory: {
+//           path: 'public'
+//         }
+//       }
+//     })
+
+//     if (err) {
+//       console.error('Failed to load plugin: ', err);
+//     }
+
+//     server.start((err) => {
+//       if (err) {
+//         throw err;
+//       }
+
+//       console.log('Server running at:', server.info.uri);
+//     });
+//   }
+// );
+
+// module.exports = server;
