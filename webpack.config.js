80426e80829c792e56279ecf91061a0f7d66e096
Aaron Panchal <aaron.panchal@appfolio.com>
2016-07-15 01:33:47 -0700

    move client dir into src

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './src/client/index.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
