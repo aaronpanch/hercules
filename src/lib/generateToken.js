0471f5c5d7a1546f603e93b4798fd2c3eb9e047f
Aaron Panchal <aaron.panchal@gmail.com>
2019-02-03 14:48:10 -0800

    Clear old repo (see v1 branch for backup)

--- src/lib/generateToken.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const config = require('config')
-    , jwt = require('jsonwebtoken');
-
-module.exports = function (payload) {
-  return jwt.sign(payload, config.secret, { expiresIn: '1d' });
-}
